aef768a65fdc5bb8255829e7298a226f1d4927e7
Carlos Alvarez <carlosprade00@gmail.com>
2022-11-02 12:19:53 +0100

    ReadOnly support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Build styles
  */
-require('./index.css').toString();
+require("./index.css").toString();
 
 /**
  * SimpleImage Tool for the Editor.js
@@ -24,8 +24,10 @@ class SimpleImage {
    *   data — previously saved data
    *   config - user config for Tool
    *   api - Editor.js API
+   *   readOnly - read-only mode flag
    */
-  constructor({ data, config, api }) {
+
+  constructor({ data, config, api, readOnly }) {
     /**
      * Editor.js API
      */
@@ -53,10 +55,10 @@ class SimpleImage {
       /**
        * Tool's classes
        */
-      wrapper: 'cdx-simple-image',
-      imageHolder: 'cdx-simple-image__picture',
-      caption: 'cdx-simple-image__caption',
-      alt: 'cdx-simple-image__alt'
+      wrapper: "cdx-simple-image",
+      imageHolder: "cdx-simple-image__picture",
+      caption: "cdx-simple-image__caption",
+      alt: "cdx-simple-image__alt",
     };
 
     /**
@@ -67,44 +69,45 @@ class SimpleImage {
       imageHolder: null,
       image: null,
       caption: null,
-      alt: null
+      alt: null,
     };
 
     /**
      * Tool's initial data
      */
     this.data = {
-      url: data.url || '',
-      caption: data.caption || '',
-      alt: data.alt || '',
+      url: data.url || "",
+      caption: data.caption || "",
+      alt: data.alt || "",
       withBorder: data.withBorder !== undefined ? data.withBorder : false,
-      withBackground: data.withBackground !== undefined ? data.withBackground : false,
+      withBackground:
+        data.withBackground !== undefined ? data.withBackground : false,
       stretched: data.stretched !== undefined ? data.stretched : false,
     };
- 
+
     /**
      * Available Image settings
      */
     this.settings = [
       {
-        name: 'withBorder',
-        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15.8 10.592v2.043h2.35v2.138H15.8v2.232h-2.25v-2.232h-2.4v-2.138h2.4v-2.28h2.25v.237h1.15-1.15zM1.9 8.455v-3.42c0-1.154.985-2.09 2.2-2.09h4.2v2.137H4.15v3.373H1.9zm0 2.137h2.25v3.325H8.3v2.138H4.1c-1.215 0-2.2-.936-2.2-2.09v-3.373zm15.05-2.137H14.7V5.082h-4.15V2.945h4.2c1.215 0 2.2.936 2.2 2.09v3.42z"/></svg>`
+        name: "withBorder",
+        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M15.8 10.592v2.043h2.35v2.138H15.8v2.232h-2.25v-2.232h-2.4v-2.138h2.4v-2.28h2.25v.237h1.15-1.15zM1.9 8.455v-3.42c0-1.154.985-2.09 2.2-2.09h4.2v2.137H4.15v3.373H1.9zm0 2.137h2.25v3.325H8.3v2.138H4.1c-1.215 0-2.2-.936-2.2-2.09v-3.373zm15.05-2.137H14.7V5.082h-4.15V2.945h4.2c1.215 0 2.2.936 2.2 2.09v3.42z"/></svg>`,
       },
       {
-        name: 'stretched',
-        icon: `<svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg"><path d="M13.568 5.925H4.056l1.703 1.703a1.125 1.125 0 0 1-1.59 1.591L.962 6.014A1.069 1.069 0 0 1 .588 4.26L4.38.469a1.069 1.069 0 0 1 1.512 1.511L4.084 3.787h9.606l-1.85-1.85a1.069 1.069 0 1 1 1.512-1.51l3.792 3.791a1.069 1.069 0 0 1-.475 1.788L13.514 9.16a1.125 1.125 0 0 1-1.59-1.591l1.644-1.644z"/></svg>`
+        name: "stretched",
+        icon: `<svg width="17" height="10" viewBox="0 0 17 10" xmlns="http://www.w3.org/2000/svg"><path d="M13.568 5.925H4.056l1.703 1.703a1.125 1.125 0 0 1-1.59 1.591L.962 6.014A1.069 1.069 0 0 1 .588 4.26L4.38.469a1.069 1.069 0 0 1 1.512 1.511L4.084 3.787h9.606l-1.85-1.85a1.069 1.069 0 1 1 1.512-1.51l3.792 3.791a1.069 1.069 0 0 1-.475 1.788L13.514 9.16a1.125 1.125 0 0 1-1.59-1.591l1.644-1.644z"/></svg>`,
       },
       {
-        name: 'withBackground',
-        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.043 8.265l3.183-3.183h-2.924L4.75 10.636v2.923l4.15-4.15v2.351l-2.158 2.159H8.9v2.137H4.7c-1.215 0-2.2-.936-2.2-2.09v-8.93c0-1.154.985-2.09 2.2-2.09h10.663l.033-.033.034.034c1.178.04 2.12.96 2.12 2.089v3.23H15.3V5.359l-2.906 2.906h-2.35zM7.951 5.082H4.75v3.201l3.201-3.2zm5.099 7.078v3.04h4.15v-3.04h-4.15zm-1.1-2.137h6.35c.635 0 1.15.489 1.15 1.092v5.13c0 .603-.515 1.092-1.15 1.092h-6.35c-.635 0-1.15-.489-1.15-1.092v-5.13c0-.603.515-1.092 1.15-1.092z"/></svg>`
+        name: "withBackground",
+        icon: `<svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.043 8.265l3.183-3.183h-2.924L4.75 10.636v2.923l4.15-4.15v2.351l-2.158 2.159H8.9v2.137H4.7c-1.215 0-2.2-.936-2.2-2.09v-8.93c0-1.154.985-2.09 2.2-2.09h10.663l.033-.033.034.034c1.178.04 2.12.96 2.12 2.089v3.23H15.3V5.359l-2.906 2.906h-2.35zM7.951 5.082H4.75v3.201l3.201-3.2zm5.099 7.078v3.04h4.15v-3.04h-4.15zm-1.1-2.137h6.35c.635 0 1.15.489 1.15 1.092v5.13c0 .603-.515 1.092-1.15 1.092h-6.35c-.635 0-1.15-.489-1.15-1.092v-5.13c0-.603.515-1.092 1.15-1.092z"/></svg>`,
       },
     ];
   }
 
   static get toolbox() {
     return {
-      title: 'Image',
-      icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>'
+      title: "Image",
+      icon: '<svg width="17" height="15" viewBox="0 0 336 276" xmlns="http://www.w3.org/2000/svg"><path d="M291 150V79c0-19-15-34-34-34H79c-19 0-34 15-34 34v42l67-44 81 72 56-29 42 30zm0 52l-43-30-56 30-81-67-66 39v23c0 19 15 34 34 34h178c17 0 31-13 34-29zM79 0h178c44 0 79 35 79 79v118c0 44-35 79-79 79H79c-44 0-79-35-79-79V79C0 35 35 0 79 0z"/></svg>',
     };
   }
 
@@ -117,20 +120,20 @@ class SimpleImage {
    * @public
    */
   render() {
-    let wrapper = this._make('div', [this.CSS.baseClass, this.CSS.wrapper]),
-      loadButton = this._make('input', [], {
-        type: 'file'
+    let wrapper = this._make("div", [this.CSS.baseClass, this.CSS.wrapper]),
+      loadButton = this._make("input", [], {
+        type: "file",
       }),
-      loader = this._make('div', this.CSS.loading),
-      imageHolder = this._make('div', this.CSS.imageHolder),
-      image = this._make('img'),
-      caption = this._make('div', [this.CSS.input, this.CSS.caption], {
-        contentEditable: 'true',
-        innerHTML: this.data.caption || ''
+      loader = this._make("div", this.CSS.loading),
+      imageHolder = this._make("div", this.CSS.imageHolder),
+      image = this._make("img"),
+      caption = this._make("div", [this.CSS.input, this.CSS.caption], {
+        contentEditable: !this.readOnly,
+        innerHTML: this.data.caption || "",
       }),
-      alt = this._make('div', [this.CSS.input, this.CSS.alt], {
-        contentEditable: 'true',
-        innerHTML: this.data.alt || ''
+      alt = this._make("div", [this.CSS.input, this.CSS.alt], {
+        contentEditable: !this.readOnly,
+        innerHTML: this.data.alt || "",
       });
 
     this.nodes.imageHolder = imageHolder;
@@ -141,8 +144,8 @@ class SimpleImage {
     this.nodes.loader = loader;
     this.nodes.loadButton = loadButton;
 
-    caption.dataset.placeholder = 'Enter a caption';
-    alt.dataset.placeholder = 'Enter a alternative text';
+    caption.dataset.placeholder = "Enter a caption";
+    alt.dataset.placeholder = "Enter a alternative text";
 
     image.onload = () => {
       wrapper.classList.remove(this.CSS.loading);
@@ -163,25 +166,25 @@ class SimpleImage {
 
     image.onerror = (e) => {
       // @todo use api.Notifies.show() to show error notification
-      console.log('Failed to load an image', e);
+      console.log("Failed to load an image", e);
     };
 
     if (this.data.url) {
       wrapper.appendChild(loader);
-      image.src = this.data.url;    }
-    else {
+      image.src = this.data.url;
+    } else {
       wrapper.appendChild(loadButton);
 
       loadButton.onchange = (e) => {
         const file = e.target.files[0];
         const url = URL.createObjectURL(file);
-        
+
         this.data = {
           url: url,
           caption: file.name,
-          alt: file.name
+          alt: file.name,
         };
-      
+
         loadButton.remove();
         loadButton = null;
       };
@@ -197,9 +200,9 @@ class SimpleImage {
    * @return {SimpleImageData}
    */
   save(blockContent) {
-    let image = blockContent.querySelector('img'),
-      caption = blockContent.querySelector('.' + this.CSS.input);
-      alt = blockContent.querySelector('.' + this.CSS.input);
+    let image = blockContent.querySelector("img"),
+      caption = blockContent.querySelector("." + this.CSS.input);
+    alt = blockContent.querySelector("." + this.CSS.input);
 
     if (!image) {
       return this.data;
@@ -208,7 +211,7 @@ class SimpleImage {
     return Object.assign(this.data, {
       url: image.src,
       caption: caption.innerHTML,
-      alt: alt.innerHTML
+      alt: alt.innerHTML,
     });
   }
 
@@ -224,10 +227,19 @@ class SimpleImage {
       caption: {
         br: true,
       },
-      alt:{
+      alt: {
         br: true,
-      }
+      },
     };
+  }
+
+  /**
+   * Notify core that read-only mode is suppoorted
+   *
+   * @returns {boolean}
+   */
+  static get isReadOnlySupported() {
+    return true;
   }
 
   /**
@@ -237,7 +249,7 @@ class SimpleImage {
    */
   onPaste(event) {
     switch (event.type) {
-      case 'tag':
+      case "tag":
         const img = event.detail.data;
 
         this.data = {
@@ -246,7 +258,7 @@ class SimpleImage {
 
         break;
 
-      case 'pattern':
+      case "pattern":
         const { data: text } = event.detail;
 
         this.data = {
@@ -254,12 +266,12 @@ class SimpleImage {
         };
         break;
 
-      case 'file':
+      case "file":
         const { file } = event.detail;
         this.data = {
           url: URL.createObjectURL(file),
           caption: file.name,
-          alt: file.name
+          alt: file.name,
         };
         break;
     }
@@ -305,11 +317,11 @@ class SimpleImage {
   static get pasteConfig() {
     return {
       patterns: {
-        image: /https?:\/\/\S+\.(gif|jpe?g|tiff|png)$/i
+        image: /https?:\/\/\S+\.(gif|jpe?g|tiff|png)$/i,
       },
-      tags: ['img'],
+      tags: ["img"],
       files: {
-        mimeTypes: ['image/*']
+        mimeTypes: ["image/*"],
       },
     };
   }
@@ -319,15 +331,15 @@ class SimpleImage {
    * @return {HTMLDivElement}
    */
   renderSettings() {
-    let wrapper = document.createElement('div');
+    let wrapper = document.createElement("div");
 
-    this.settings.forEach(tune => {
-      let el = document.createElement('div');
+    this.settings.forEach((tune) => {
+      let el = document.createElement("div");
 
       el.classList.add(this.CSS.settingsButton);
       el.innerHTML = tune.icon;
 
-      el.addEventListener('click', () => {
+      el.addEventListener("click", () => {
         this._toggleTune(tune.name);
         el.classList.toggle(this.CSS.settingsButtonActive);
       });
@@ -337,14 +349,14 @@ class SimpleImage {
       wrapper.appendChild(el);
     });
     return wrapper;
-  };
+  }
 
   removed() {
-    if (this.data.url && this.data.url.startsWith('blob:')) {
+    if (this.data.url && this.data.url.startsWith("blob:")) {
       URL.revokeObjectURL(this.data.url);
     }
   }
-  
+
   /**
    * Helper for making Elements with attributes
    *
@@ -383,10 +395,15 @@ class SimpleImage {
    * @private
    */
   _acceptTuneView() {
-    this.settings.forEach(tune => {
-      this.nodes.imageHolder.classList.toggle(this.CSS.imageHolder + '--' + tune.name.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`), !!this.data[tune.name]);
+    this.settings.forEach((tune) => {
+      this.nodes.imageHolder.classList.toggle(
+        this.CSS.imageHolder +
+          "--" +
+          tune.name.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`),
+        !!this.data[tune.name]
+      );
 
-      if (tune.name === 'stretched') {
+      if (tune.name === "stretched") {
         this.api.blocks.stretchBlock(this.blockIndex, !!this.data.stretched);
       }
     });
